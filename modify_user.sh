200fbc024c7259517b3d173613ba4e641bedd2f3
Harry Chen <czy88840616@gmail.com>
2018-11-23 17:53:15 +0800

    chore: modify sh

--- modify_user.sh
+++ modify_user.sh
@@ -6,7 +6,7 @@ then
 export GIT_AUTHOR_NAME="Harry Chen"
 export GIT_AUTHOR_EMAIL="czy88840616@gmail.com"
 fi
-' 8a26e2e..HEAD
+' ref..HEAD
 
 git filter-branch -f --env-filter '
 if [ "$GIT_COMMITTER_NAME" = "张挺" ]
@@ -14,4 +14,4 @@ then
 export GIT_COMMITTER_NAME="Harry Chen"
 export GIT_COMMITTER_EMAIL="czy88840616@gmail.com"
 fi
-' 8a26e2e..HEAD
+' ref..HEAD
