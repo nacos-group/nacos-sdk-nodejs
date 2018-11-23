dcc227b95e18abe09189bac4d05bffa243488cb5
Harry Chen <czy88840616@gmail.com>
2018-11-23 17:51:21 +0800

    chore: modify sh

--- modify_user.sh
+++ modify_user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git filter-branch --env-filter '
+git filter-branch -f --env-filter '
 if [ "$GIT_AUTHOR_NAME" = "oldName" ]
 then
 export GIT_AUTHOR_NAME="newName"
@@ -8,7 +8,7 @@ export GIT_AUTHOR_EMAIL="newEmail"
 fi
 ' 8a26e2e..HEAD
 
-git filter-branch --env-filter '
+git filter-branch -f --env-filter '
 if [ "$GIT_COMMITTER_NAME" = "oldName" ]
 then
 export GIT_COMMITTER_NAME="newName"
