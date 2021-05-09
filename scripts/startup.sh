3198955b30ba3a89c292eca5c5d5d38ef425a729
Harry Chen <czy88840616@gmail.com>
2021-05-09 16:44:52 +0800

    chore: update

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,7 +5,9 @@ java -version
 curl -o nacos-server-2.0.1.zip https://github.com/alibaba/nacos/releases/download/2.0.1/nacos-server-2.0.1.zip
 jar xvf nacos-server-2.0.1.zip
 
-chmod 755 ./nacos/bin/startup.sh
+ls ./nacos
+
+chmod +x ./nacos/bin/startup.sh
 nohup ./nacos/bin/startup.sh -m standalone 2>&1 &
 sleep 30
 cat nohup.out
