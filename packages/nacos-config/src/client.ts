24538e1bd1e4f859e1e9f2bba4805160d507635d
cxhello <caixiaohuichn@gmail.com>
2026-05-21 22:46:40 +0800

    feat(nacos-config): add transport strategy pattern (gRPC default, HTTP fallback)
    Signed-off-by: cxhello <caixiaohuichn@gmail.com>

--- packages/nacos-config/src/client.ts
+++ packages/nacos-config/src/client.ts
@@ -31,6 +31,8 @@ import { CURRENT_UNIT, DEFAULT_OPTIONS } from './const';
 import { checkParameters } from './utils';
 import { HttpAgent } from './http_agent';
 import { Configuration } from './configuration';
+import { GrpcConfigProxy } from './grpc_config_proxy';
+import { GrpcConnection, GrpcTransportClient } from 'nacos-common';
 import * as assert from 'assert';
 
 const Base = require('sdk-base');
@@ -41,8 +43,12 @@ export class DataClient extends Base implements BaseClient {
   private clients: Map<string, IClientWorker>;
   private configuration: IConfiguration;
   protected snapshot: ISnapshot;
-  protected serverMgr: IServerListManager;
+  protected serverMgr: IServerListManager | null;
   protected httpAgent;
+  private _transport: 'grpc' | 'http';
+  private _grpcConnection: GrpcConnection | null;
+  private _grpcTransportClient: GrpcTransportClient | null;
+  private _grpcConfigProxy: GrpcConfigProxy | null;
 
   constructor(options: ClientOptions) {
     if(!options.endpoint && !options.serverAddr) {
@@ -52,21 +58,74 @@ export class DataClient extends Base implements BaseClient {
     options = Object.assign({}, DEFAULT_OPTIONS, options);
     super(options);
     this.configuration = this.options.configuration = new Configuration(options);
+    this._transport = (options.transport === 'grpc') ? 'grpc' : 'http';
+    this._grpcConnection = null;
+    this._grpcTransportClient = null;
+    this._grpcConfigProxy = null;
 
     this.snapshot = this.getSnapshot();
-    this.serverMgr = this.getServerListManager();
-    const CustomHttpAgent = this.configuration.get(ClientOptionKeys.HTTP_AGENT);
-    this.httpAgent = CustomHttpAgent ? new CustomHttpAgent({ configuration: this.configuration }) : new HttpAgent({ configuration: this.configuration });
+    (<any>this.snapshot).on('error', err => this.throwError(err));
 
-    this.configuration.merge({
-      snapshot: this.snapshot,
-      serverMgr: this.serverMgr,
-      httpAgent: this.httpAgent,
-    });
+    if (this._transport === 'grpc') {
+      // gRPC mode: skip ServerListManager and HttpAgent; set up gRPC stack
+      this.serverMgr = null;
+      this.httpAgent = null;
+
+      this.configuration.merge({
+        snapshot: this.snapshot,
+      });
+
+      // Normalise serverAddr to a string array
+      const rawAddr = options.serverAddr;
+      let serverList: string[];
+      if (Array.isArray(rawAddr)) {
+        serverList = rawAddr;
+      } else if (rawAddr) {
+        serverList = [ rawAddr ];
+      } else {
+        serverList = [ `${options.endpoint || 'localhost'}:8848` ];
+      }
+
+      const logger = (this.options as any).logger || console;
+      this._grpcConnection = new GrpcConnection({
+        serverList,
+        namespace: options.namespace,
+        ssl: options.ssl,
+        logger,
+        accessKey: options.accessKey,
+        secretKey: options.secretKey,
+        username: options.username,
+        password: options.password,
+      });
+
+      this._grpcTransportClient = new GrpcTransportClient(this._grpcConnection);
+
+      this._grpcConfigProxy = new GrpcConfigProxy({
+        transportClient: this._grpcTransportClient,
+        namespace: options.namespace,
+        logger,
+      });
+
+      this._grpcConfigProxy.on('configChanged', ({ dataId, group, tenant }) => {
+        // Emit so subscribers can be notified
+        this.emit('configChanged', { dataId, group, tenant });
+      });
+    } else {
+      // HTTP mode: original initialization
+      this.serverMgr = this.getServerListManager();
+      (<any>this.serverMgr).on('error', err => this.throwError(err));
+
+      const CustomHttpAgent = this.configuration.get(ClientOptionKeys.HTTP_AGENT);
+      this.httpAgent = CustomHttpAgent ? new CustomHttpAgent({ configuration: this.configuration }) : new HttpAgent({ configuration: this.configuration });
+
+      this.configuration.merge({
+        snapshot: this.snapshot,
+        serverMgr: this.serverMgr,
+        httpAgent: this.httpAgent,
+      });
+    }
 
     this.clients = new Map();
-    (<any>this.snapshot).on('error', err => this.throwError(err));
-    (<any>this.serverMgr).on('error', err => this.throwError(err));
     this.ready(true);
   }
 
@@ -83,6 +142,9 @@ export class DataClient extends Base implements BaseClient {
    * @return {String} currentUnit
    */
   async getCurrentUnit() {
+    if (!this.serverMgr) {
+      return 'gRPC';
+    }
     return await this.serverMgr.getCurrentUnit();
   }
 
@@ -91,6 +153,9 @@ export class DataClient extends Base implements BaseClient {
    * @return {Array} units
    */
   async getAllUnits() {
+    if (!this.serverMgr) {
+      return [];
+    }
     return await this.serverMgr.fetchUnitLists();
   }
 
@@ -136,8 +201,11 @@ export class DataClient extends Base implements BaseClient {
    *   - {Stirng} unit - which unit you want to connect, default is current unit
    * @return {String} value
    */
-  async getConfig(dataId, group, options) {
+  async getConfig(dataId, group, options?) {
     checkParameters(dataId, group);
+    if (this._grpcConfigProxy) {
+      return await this._grpcConfigProxy.getConfig(dataId, group, this.configuration.get(ClientOptionKeys.NAMESPACE));
+    }
     const client = this.getClient(options);
     return await client.getConfig(dataId, group);
   }
@@ -164,6 +232,14 @@ export class DataClient extends Base implements BaseClient {
    */
   async publishSingle(dataId, group, content, options?: UnitOptions) {
     checkParameters(dataId, group);
+    if (this._grpcConfigProxy) {
+      return await this._grpcConfigProxy.publishSingle(
+        dataId, group,
+        this.configuration.get(ClientOptionKeys.NAMESPACE),
+        content,
+        options && options.type
+      );
+    }
     const client = this.getClient(options);
     return await client.publishSingle(dataId, group, content, options);
   }
@@ -176,8 +252,11 @@ export class DataClient extends Base implements BaseClient {
    *   - {Stirng} unit - which unit you want to connect, default is current unit
    * @return {Boolean} success
    */
-  async remove(dataId, group, options) {
+  async remove(dataId, group, options?) {
     checkParameters(dataId, group);
+    if (this._grpcConfigProxy) {
+      return await this._grpcConfigProxy.remove(dataId, group, this.configuration.get(ClientOptionKeys.NAMESPACE));
+    }
     const client = this.getClient(options);
     return await client.remove(dataId, group);
   }
@@ -262,7 +341,15 @@ export class DataClient extends Base implements BaseClient {
   }
 
   close() {
-    this.serverMgr.close();
+    if (this._grpcConfigProxy) {
+      this._grpcConfigProxy.close();
+    }
+    if (this._grpcConnection) {
+      this._grpcConnection.close();
+    }
+    if (this.serverMgr) {
+      this.serverMgr.close();
+    }
     for (const client of this.clients.values()) {
       client.close();
     }
